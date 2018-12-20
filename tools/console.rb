0f7a0722d041ef3ee8f3dbddfae328425dbab999
lsamano <leizl.samano@gmail.com>
2018-12-19 21:24:24 -0500

    Boating School complete.

--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,18 @@
 require_relative '../config/environment'
 
 # Declare your variables here~~~
+spongebob = Student.new("SpongeBob", "SquarePants")
+patrick = Student.new("Patrick", "Star")
+mrkrabs = Student.new("Eugene", "Krabs")
 
+mrspuff = Instructor.new("Mrs. Puff")
+plankton = Instructor.new("Plankton")
+sandy = Instructor.new("Sandy Cheeks")
+
+test1 = BoatingTest.new(spongebob, "Driving Test", "FLOOR IT? FLOOR IT!!!!", mrspuff)
+test2 = BoatingTest.new(spongebob, "Parallel Parking", "Pass, somehow", sandy)
+test3 = BoatingTest.new(patrick, "Driving Test", "PASSED", plankton)
+test4 = BoatingTest.new(mrkrabs, "Signaling", "Passed", mrspuff)
 
 # don't remove!
 binding.pry
-0
